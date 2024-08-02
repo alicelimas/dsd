12fa2ded577398bb8402116c2f8032db5cb605f4
Alice <105982546+alicelimas@users.noreply.github.com>
2024-08-02 10:55:45 -0300

    Update servidor.py

--- atividade 03 - rmi/servidor.py
+++ atividade 03 - rmi/servidor.py
@@ -5,17 +5,19 @@ class ReservaDeSalas:
     def __init__(self):
         self.salas = {"Sala 1": True, "Sala 2": True, "Sala 3": True, "Sala 4": True}
         self.reservas = {}
-        self.clientes = []
+        self.clientes = {}
 
-    def registrar_cliente(self, cliente_uri):
-        if cliente_uri not in self.clientes:
-            self.clientes.append(cliente_uri)
-            print(f"Cliente registrado: {cliente_uri}")
+    def registrar_cliente(self, nome_usuario):
+        if nome_usuario in self.clientes:
+            return "Nome de usuário já existe."
+        self.clientes[nome_usuario] = True
+        print(f"Cliente registrado: {nome_usuario}")
+        return "Registrado com sucesso."
 
-    def desregistrar_cliente(self, cliente_uri):
-        if cliente_uri in self.clientes:
-            self.clientes.remove(cliente_uri)
-            print(f"Cliente desregistrado: {cliente_uri}")
+    def desregistrar_cliente(self, nome_usuario):
+        if nome_usuario in self.clientes:
+            del self.clientes[nome_usuario]
+            print(f"Cliente desregistrado: {nome_usuario}")
 
     def reservar_sala(self, nome_sala, usuario):
         if not self.salas.get(nome_sala):
@@ -24,7 +26,6 @@ class ReservaDeSalas:
             return "Você já reservou essa sala."
         self.salas[nome_sala] = False
         self.reservas[nome_sala] = usuario
-        self.notificar_clientes(f"{usuario} reservou {nome_sala}.")
         return "Sala reservada com sucesso."
 
     def cancelar_reserva(self, nome_sala, usuario):
@@ -32,19 +33,10 @@ class ReservaDeSalas:
             return "Você não tem uma reserva para essa sala."
         self.salas[nome_sala] = True
         del self.reservas[nome_sala]
-        self.notificar_clientes(f"{usuario} cancelou a reserva de {nome_sala}.")
         return "Reserva cancelada com sucesso."
 
     def obter_salas(self):
         return self.salas
-
-    def notificar_clientes(self, mensagem):
-        for cliente_uri in self.clientes:
-            try:
-                cliente = Pyro4.Proxy(cliente_uri)
-                cliente.receber_notificacao(mensagem)
-            except Exception as e:
-                print(f"Erro ao notificar cliente {cliente_uri}: {e}")
 
 def main():
     host = "10.3.5.11" 
